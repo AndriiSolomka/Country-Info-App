4ab3095f57719f993b56d4bdd6636facaf3882cc
andrii_solomka <andriissolomka@gmail.com>
2025-03-31 17:21:52 +0300

    Feat: implement user module with controller and service

--- src/common/exceptions/custom-http.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class CountryHttpException extends HttpException {
-  constructor() {
-    super('Failed to fetch available countries:', HttpStatus.BAD_GATEWAY);
-  }
-}
